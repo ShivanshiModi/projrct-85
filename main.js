de45d350d5c327ffe7ed8d5329f947dd4efbdfb3
ShivanshiModi <87198158+ShivanshiModi@users.noreply.github.com>
2022-04-05 13:28:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ function my_keydown(e)
 
 function up()
 {
-	if(greencar_y >= 0)
+	if(greencar_y <= 0)
 	{
 		greencar_y = greencar_y - 10;
 		console.log("When up arrow is pressed, x =" + greencar_x + "| y =" + greencar_y);
@@ -86,7 +86,7 @@ function down()
 
 function left()
 {
-	if(greencar_x >= 0)
+	if(greencar_x <= 0)
 	{
 		greencar_x = greencar_x - 10;
 		console.log("When down arrow is pressed, x =" + greencar_x + "| y =" + greencar_y);
